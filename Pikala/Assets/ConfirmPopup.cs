86d28f0a2e56088ef6b248e7399dc7e14d2b29ac
pontura <pontura@gmail.com>
2016-11-03 12:40:51 -0300

    new repo

---
+++ Pikala/Assets/ConfirmPopup.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ConfirmPopup : MonoBehaviour {
+
+    public GameObject panel;
+
+    public Text title;
+
+    public Text field1;
+    public Text field2;
+    public Text field3;
+
+    public int result;
+    public List<string> nums;
+    private System.Action functionListener;
+
+    void Start () {
+        panel.SetActive(false);
+    }
+    public void Open(System.Action functionListener)
+    {
+        this.functionListener = functionListener;
+        nums.Clear();
+        int num1 = Random.Range(3, 8);
+        int num2 = Random.Range(3, 8);
+
+        title.text = num1.ToString() +  "x" + num2.ToString();
+
+        result = num1 * num2;
+
+        nums.Add ( result.ToString() );
+        nums.Add ( (result - Random.Range(2,12)).ToString() );
+        nums.Add ( (result + Random.Range(2, 12 )).ToString());
+
+        Utils.ShuffleListTexts(nums);
+
+        field1.text = nums[0];
+        field2.text = nums[1];
+        field3.text = nums[2];
+
+        panel.SetActive(true);
+    }
+    public void Close()
+    {
+        panel.SetActive(false);
+    }
+    public void Clicked(int id)
+    {
+        string toCheckResult = "";
+        switch(id)
+        {
+            case 1: toCheckResult = field1.text; break;
+            case 2: toCheckResult = field2.text; break;
+            case 3: toCheckResult = field3.text; break;
+        }
+        if (result.ToString() == toCheckResult)
+            Go();
+        Close();
+    }
+    void Go()
+    {
+        print("OK");
+        functionListener();
+    }
+}
---
+++ Pikala/Assets/ProgressData.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ProgressData : MonoBehaviour {
+
+    public List<string> well1;
+    public List<string> well2;
+    public List<string> well3;
+
+    public List<string> lastWrongWords;
+
+    void Start () {        
+        Events.OnAddWordToList += OnAddWordToList;
+        //GetSavedWords();
+    }
+    void GetSavedWords()
+    {
+        for (int a = 0; a < 3; a++)
+        {
+            for (int b = 0; b < 40; b++)
+            {
+                string word = PlayerPrefs.GetString("well_" + a + 1 + "_" + b + 1, "");
+                if (word != "")
+                    AddWordTo(a + 1, word);
+            }
+        }
+    }
+    void AddWordTo(int arrId, string word)
+    {
+        switch(arrId)
+        {
+            case 1: well1.Add(word); break;
+            case 2: well2.Add(word); break;
+            case 3: well3.Add(word); break;
+        }
+    }
+    void OnAddWordToList(GameData.types type, int id)
+    {
+
+    }
+
+}
